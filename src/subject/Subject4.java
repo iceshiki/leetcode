e43800f46e503ae4c4fc0d5bf110c8e8ae1aecbf
chenjianchao <418912649@qq.com>
2023-10-31 16:15:19 +0800

    subject 4

---
+++ src/subject/Subject4.java
@@ -0,0 +1,73 @@
+package subject;
+
+
+/**
+ * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ * <p>
+ * 算法的时间复杂度应该为 O(log (m+n)) 。
+ * <p>
+ * 示例 1：
+ * 输入：nums1 = [1,3], nums2 = [2]
+ * 输出：2.00000
+ * 解释：合并数组 = [1,2,3] ，中位数 2
+ * <p>
+ * 示例 2：
+ * 输入：nums1 = [1,2], nums2 = [3,4]
+ * 输出：2.50000
+ * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+ * <p>
+ * 提示：
+ * nums1.length == m
+ * nums2.length == n
+ * 0 <= m <= 1000
+ * 0 <= n <= 1000
+ * 1 <= m + n <= 2000
+ * -106 <= nums1[i], nums2[i] <= 106
+ *
+ * @level hard
+ */
+public class Subject4 {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) {
+            int[] tmp = nums1;
+            nums1 = nums2;
+            nums2 = tmp;
+        }
+
+        int medianIdx = m + (n - m + 1) / 2;
+        int left = 0;
+        int right = m;
+
+
+        while (left < right) {
+            int i = left + (right - left + 1) / 2;
+            int j = medianIdx - i;
+            if (nums1[i - 1] > nums2[j]) {
+//                nextSpace[left, i-1]
+                right = i - 1;
+            } else {
+                // nextSpace[i, right]
+                left = i;
+            }
+        }
+
+        int i = left;
+        int j = medianIdx - i;
+
+        int nums1LeftMax = i == 0 ? Integer.MIN_VALUE : nums1[i - 1];
+        int nums1RightMin = i == m ? Integer.MAX_VALUE : nums1[i];
+        int nums2LeftMax = j == 0 ? Integer.MIN_VALUE : nums2[j - 1];
+        int nums2RightMin = j == n ? Integer.MAX_VALUE : nums2[j];
+
+        if (((m + n) % 2) == 1) {
+            return Math.max(nums1LeftMax, nums2LeftMax);
+        } else {
+            return (double) ((Math.max(nums1LeftMax, nums2LeftMax) + Math.min(nums1RightMin, nums2RightMin))) / 2;
+        }
+
+    }
+
+}
